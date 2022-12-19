92ef29b0aae0bcb00661c4352e808dd2764e3459
Renuka-Jain <renuka.jain@estudiantat.upc.edu>
2022-12-19 01:44:42 +0100

    splash ok

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Rect } from 'react-native-svg';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import routes from './src/config/routes';
+import React, { useEffect } from 'react';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={'Home'}>
+        {routes.map((r, i) => (
+          <Stack.Screen key={i} name={r.name}>
+            {(props) => <r.component nameProp={r.name} {...props} />}
+
+          </Stack.Screen>
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+import IRouteProps from "../library/RouteProp";
+import HomeScreen from "../screens/Home";
+
+const routes: IRouteProps[] = [
+    {
+        name: 'Home',
+        component: HomeScreen
+    }
+]
+
+export default routes;
